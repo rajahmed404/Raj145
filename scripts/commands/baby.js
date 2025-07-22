b9d15af505cc7f40b48918c3da52f4e1ffc1702c
rajahmed404 <mdsohidul86145@gmail.com>
2025-07-22 15:54:27 +0600

    Update baby.js

--- scripts/commands/baby.js
+++ scripts/commands/baby.js
@@ -156,7 +156,7 @@ try{
 module.exports.handleEvent = async function ({ api, event }) {
 try{
  const body = event.body ? event.body.toLowerCase() : ""
- if(body.startsWith("baby") || body.startsWith("bby") || body.startsWith("/bot")){
+ if(body.startsWith("বাবু") || body.startsWith("bby") || body.startsWith("/bot")){
  const arr = body.replace(/^\S+\s*/, "")
  if(!arr) {
  await api.sendMessage("hum xan bolo ami asi ", event.threadID, (error, info) => {
